134d010dd9a6505213844832413ee2110ab92005
Nexxxit <varsik.arsik@mail.ru>
2025-04-21 21:16:50 +0500

    feat: add Label component

---
+++ src/components/label/Label.tsx
@@ -0,0 +1,17 @@
+interface LabelProps {
+    htmlFor: string;
+    labelText?: string;
+    type: string;
+    placeholderText?: string;
+    labelClassName?: string;
+    inputClassName?: string;
+}
+
+export default function Label({htmlFor, labelText, type, placeholderText, labelClassName, inputClassName}: LabelProps) {
+    return (
+        <label className={`${labelClassName} flex flex-col`} htmlFor={htmlFor}>
+            {labelText}
+            <input className={`${inputClassName} `} id={htmlFor} type={type} placeholder={placeholderText} />
+        </label>
+    );
+}
