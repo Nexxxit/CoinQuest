a4b7c9e441c53e9f7824c59c2247675432b39b31
Nexxxit <varsik.arsik@mail.ru>
2025-04-30 23:23:48 +0500

    feat: add new props to Label

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,17 +1,38 @@
 interface LabelProps {
-    htmlFor: string;
-    labelText?: string;
-    type: string;
-    placeholderText?: string;
-    labelClassName?: string;
-    inputClassName?: string;
+  htmlFor: string;
+  labelText?: string;
+  type: string;
+  placeholderText?: string;
+  labelClassName?: string;
+  inputClassName?: string;
+  required?: boolean;
+  value?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Label({htmlFor, labelText, type, placeholderText, labelClassName, inputClassName}: LabelProps) {
-    return (
-        <label className={`${labelClassName} flex flex-col`} htmlFor={htmlFor}>
-            {labelText}
-            <input className={`${inputClassName} border rounded-lg placeholder:p-1`} id={htmlFor} type={type} placeholder={placeholderText} />
-        </label>
-    );
+export default function Label({
+  htmlFor,
+  labelText,
+  type,
+  placeholderText,
+  labelClassName,
+  inputClassName,
+  required,
+  onChange,
+  value,
+}: LabelProps) {
+  return (
+    <label className={`${labelClassName} flex flex-col`} htmlFor={htmlFor}>
+      {labelText}
+      <input
+        className={`${inputClassName} border-2 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-400 transition-colors`}
+        id={htmlFor}
+        type={type}
+        placeholder={placeholderText}
+        onChange={onChange}
+        required={required}
+        value={value}
+      />
+    </label>
+  );
 }
