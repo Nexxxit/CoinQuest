1d7c74a0d552e23f05b43fa4318816b7045d8cac
Nexxxit <varsik.arsik@mail.ru>
2025-04-30 23:21:44 +0500

    feat: add new props to Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,11 +2,12 @@ interface ButtonProps {
     label: string;
     className?: string;
     onClick?: () => void;
+    type?: string;
 }
 
-export default function Button({label, className, onClick}: ButtonProps) {
+export default function Button({label, className, onClick, type}: ButtonProps) {
     return (
-        <button className={`${className} shadow p-1 rounded-lg bg-indigo-500 hover:bg-blue-500`} onClick={onClick}>
+        <button type={type = "button"} className={`${className} shadow p-1 rounded-lg bg-indigo-500 hover:bg-blue-500`} onClick={onClick}>
             {label}
         </button>
     );
