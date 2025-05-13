94506ee98db290a2e6f367537c369e560fc53634
Nexxxit <varsik.arsik@mail.ru>
2025-05-13 21:11:35 +0500

    fix: update default type

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,12 +2,12 @@ interface ButtonProps {
     label: string;
     className?: string;
     onClick?: () => void;
-    type?: string;
+    type?: "button" | "submit" | "reset";
 }
 
-export default function Button({label, className, onClick, type}: ButtonProps) {
+export default function Button({label, className, onClick, type = "button"}: ButtonProps) {
     return (
-        <button type={type = "button"} className={`${className} shadow p-1 rounded-lg bg-indigo-500 hover:bg-blue-500`} onClick={onClick}>
+        <button type={type} className={`${className} cursor-pointer shadow p-1 rounded-lg bg-indigo-500 hover:bg-blue-500`} onClick={onClick}>
             {label}
         </button>
     );
