853f80ac29c4e26cd3efe9f0b94259311666488a
Nexxxit <varsik.arsik@mail.ru>
2025-05-22 16:26:11 +0500

    feat: update formatedNumberField function

--- src/components/expenseForm/ExpenseForm.tsx
+++ src/components/expenseForm/ExpenseForm.tsx
@@ -1,28 +1,32 @@
-import Label from "../label/Label";
+import Field from "../field/Field";
 import Dropdown from "../dropdown/Dropdown";
 import Button from "../button/Button";
 import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import transaction from "../../stores/transaction";
 
+// type CategoryKeywords = {
+//   [category: string]: string[];
+// };
+
 export default observer(function ExpenseForm() {
   const [amount, setAmount] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
-  const [category, setCategory] = useState("other");
+  const [category, setCategory] = useState("Другое");
   const [type, setType] = useState<"income" | "expense">("expense");
 
   const categoryOption = [
     {
-      value: "transport",
+      value: "Транспорт",
       label: "Транспорт",
     },
     {
-      value: "food",
+      value: "Еда",
       label: "Еда",
     },
     {
-      value: "other",
+      value: "Другое",
       label: "Другое",
     },
   ];
@@ -55,46 +59,74 @@ export default observer(function ExpenseForm() {
     setAmount("");
     setDescription("");
     setDate(new Date().toISOString().split("T")[0]);
-    setCategory("other");
+    setCategory("Другое");
+  };
+
+  // const categoryKeywords: CategoryKeywords = {
+  //   Транспорт: ["такси", "uber", "метро", "автобус"],
+  //   Еда: ["кафе", "ресторан", "продукты", "кофе"],
+  // };
+
+  const formatedNumberField = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const cleanedValue = value.replace(/[^\d]/g, "");
+
+    const formattedValue = Number(cleanedValue).toLocaleString("ru-RU") 
+
+    setAmount(formattedValue);
   };
 
   return (
-    <form className="flex flex-col gap-3 p-2 flex-1" onSubmit={handleSubmit}>
+    <form
+      className="flex flex-col gap-3 p-2 flex-1 bg-indigo-300 rounded-xl"
+      onSubmit={handleSubmit}
+    >
       <div className="relative">
-        <Label
-          type="number"
-          labelText="Сумма:"
-          htmlFor="amount"
-          placeholderText="1500"
-          required={true}
-          onChange={(e) => setAmount(e.target.value)}
-          value={amount}
-        />
-        <span className="absolute right-4 top-9 text-gray-500">₽</span>
-      </div>
-      <Label
+        <Field
         type="text"
-        labelText="Описание"
-        htmlFor="description"
-        placeholderText="Поездка на такси"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
+        labelText="Сумма:"
+        htmlFor="amount"
+        placeholderText="1500 ₽"
+        required={true}
+        onChange={formatedNumberField}
+        value={amount}
+        title="Введите сумму!"
+        inputClassName="pr-12"
       />
+      <span className="absolute right-4 top-1/2 transform">₽</span>
+      </div>
+      <label className="flex flex-col" htmlFor="description">
+        Описание:
+        <textarea
+          id="description"
+          className="border-2 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-400 transition-colors"
+          placeholder="Поездка на такси"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          value={description}
+          maxLength={256}
+        ></textarea>
+      </label>
       <Dropdown
+        htmlFor="category"
+        labelText="Категория:"
         value={category}
         onChange={(e) => setCategory(e.target.value)}
         options={categoryOption}
         name="category"
       />
-      <Label
+      <Field
         type="date"
-        labelText="Дата"
+        labelText="Дата:"
         htmlFor="date"
         placeholderText="12.02.2025"
         value={date}
         onChange={(e) => setDate(e.target.value)}
       />
       <Dropdown
+        htmlFor="type"
+        labelText="Тип:"
         value={type}
         onChange={(e) => setType(e.target.value as "income" | "expense")}
         options={typeOfTransactionOption}
